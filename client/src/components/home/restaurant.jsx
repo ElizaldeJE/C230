15b280ae6bcd211c0459b3ac92819ccf8257a3cd
Javier Elizalde <javelisol@gmail.com>
2018-04-24 18:06:51 -0500

    as a test got all the names rendering in the localhost, it is also connected to the server. No bugs at the moment

---
+++ client/src/components/home/restaurant.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class RestaurantList extends React.Component {
+  constructor(props){
+  super(props)
+
+    this.state = {
+      people: [],
+      name: ""
+    };
+};
+    componentDidMount() {
+      axios.get(`https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json`)
+        .then(res => {
+          const people = res.data;
+          this.setState({ people });
+        })
+    }
+
+    render() {
+      return (
+        <ul>
+          { this.state.people.map(person => <li>{person.name}</li>)}
+        </ul>
+      )
+    }
+  }
